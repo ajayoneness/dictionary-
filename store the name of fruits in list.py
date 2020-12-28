b05c179599df3e0ec56c9746e0bb75d2a437f314
ajayoneness <42382145+ajayoneness@users.noreply.github.com>
2020-12-28 12:15:01 +0545

    Ajay pandit

---
+++ store the name of fruits in list.py
@@ -0,0 +1,8 @@
+f1 = input("enter fruits name 1 : ")
+f2 = input("enter fruits name 2 : ")
+f3 = input("enter fruits name 3 : ")
+f4 = input("enter fruits name 4 : ")
+f5 = input("enter fruits name 5 : ")
+
+fruits= [f1,f2,f3,f4,f5]
+print (fruits)
